295fcaa228055733add01f3f1f4145627f7eb36b
gavazcal <2392@holbertonschool.com>
2021-02-03 14:51:25 +0000

    adding all mandatory tasks and 1 advanced task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints an output
+ * Return: 0
+ */
+
+int main()
+{
+	char str1[49];
+
+	strcpy(str1, "Programming is like building a multilingual puzzle");
+	puts(str1);
+
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints a string
+ * Return: 0
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
