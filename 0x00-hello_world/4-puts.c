ebd6b09988c09dde9d4b5f9c442c0b10745d092c
gavazcal <2392@holbertonschool.com>
2021-02-03 16:12:05 +0000

    fixing betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,16 @@
 
 /**
  * main - prints an output
+ *
  * Return: 0
  */
 
-int main()
+int main(void)
 {
 	char str1[49];
 
 	strcpy(str1, "Programming is like building a multilingual puzzle");
 	puts(str1);
 
-	return(0);
+	return (0);
 }
