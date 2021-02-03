f4d03931dcc78d140fcb3d62772df9621fefd0e9
gavazcal <2392@holbertonschool.com>
2021-02-03 16:13:30 +0000

    adding newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char str1[49];
 
-	strcpy(str1, "Programming is like building a multilingual puzzle");
+	strcpy(str1, "Programming is like building a multilingual puzzle\n");
 	puts(str1);
 
 	return (0);
