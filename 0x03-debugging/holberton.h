a5382cb58407fada1ca21f25de108d6c4ae88fb1
gavazcal <2392@holbertonschool.com>
2021-02-09 14:19:26 +0000

    adding header file

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,13 @@
+/*INCLUDE GUARD*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/*LIBRARIES*/
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/*PROTOTYPES*/
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif
