56b041a1c227dea95427d60eacd9dd5b30221c8f
gavazcal <2392@holbertonschool.com>
2021-02-09 16:46:24 +0000

    simplified way overcomplicated code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,28 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int TensHours, Hours, TensMinutes, Minutes;
+	int Hours, Minutes;
 
-	for (TensHours = 0; TensHours <= 2; TensHours++)
+	for (Hours = 0; Hours <= 23; Hours++)
 	{
-		for (Hours = 0; Hours <= 9; Hours++)
+		for (Minutes = 0; Minutes <= 59; Minutes++)
 		{
-			_putchar(TensHours);
-			_putchar(Hours);
-			if (TensHours == 2 && Hours == 4)
-				break;
-			for (TensMinutes = 0; TensMinutes <= 5; TensMinutes++)
-			{
-				for (Minutes = 0; Minutes <= 9; Minutes++)
-				{
-					_putchar(TensHours + '0');
-					_putchar(Hours + '0');
-					_putchar(':');
-					_putchar(TensMinutes + '0');
-					_putchar(Minutes + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar(Hours / 10 + '0');
+			_putchar(Hours % 10 + '0');
+			_putchar(':');
+			_putchar(Minutes / 10 + '0');
+			_putchar(Minutes % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
