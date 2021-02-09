bfea8fd6a3b750223b1bbc201b1492b7155d52ed
gavazcal <2392@holbertonschool.com>
2021-02-09 15:35:55 +0000

    Adding advanced task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * main - prints the sum of even fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long int counter;
+	long int first_digit = 1;
+	long int second_digit = 2;
+	long int fibonacci = 3;
+
+	counter = second_digit;
+
+	while (fibonacci < 4000000)
+	{
+		fibonacci = first_digit + second_digit;
+		first_digit = second_digit;
+		second_digit = fibonacci;
+		if ((second_digit < 4000000) && (second_digit %2 == 0))
+			counter += second_digit;
+	}
+	printf("%lu\n", fibonacci);
+	return (0);
+}
