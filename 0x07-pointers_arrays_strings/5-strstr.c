e1380379c746f76d9013d54718de1b1378e93654
gavazcal <2392@holbertonschool.com>
2021-02-22 19:58:54 +0000

    Adding task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: the place to look for a substring
+ * @needle: the substring
+ * Return: a pointer to the beginning of needle
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int idx, idx_two;
+
+	if (*needle == '\0' || *needle == 0)
+	{
+		return (haystack);
+	}
+	for (idx = 0; haystack[idx] != '\0'; idx++)
+	{
+		for (idx_two = 0; haystack[idx + idx_two] == needle[idx_two]; idx_two++)
+		{
+			if (needle[idx_two] == '\0')
+				return (&haystack[idx]);
+		}
+	}
+	return ('\0');
+}
