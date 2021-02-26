bce4ab4f1338fd5eb6e9cefafe6a0008896df06b
gavazcal <2392@holbertonschool.com>
2021-02-26 16:18:43 +0000

    adding task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
