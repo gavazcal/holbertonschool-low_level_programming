0ac793f2f34fdb473eba38e71fab9152d660f90e
gavazcal <2392@holbertonschool.com>
2021-02-28 17:50:34 +0000

    include guard

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,17 +1,16 @@
-/*include guard*/
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 /*PROTOTYPES*/
 int _putchar(char c);
-void _islower(void);
-void _isalpha(void);
-void _abs(void);
-void _isupper(void);
-void _isdigit(void);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-void _strcpy(void);
-void _atoi(void);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -21,6 +20,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-void _strstr(void);
-
+char *_strstr(char *haystack, char *needle);
 #endif
