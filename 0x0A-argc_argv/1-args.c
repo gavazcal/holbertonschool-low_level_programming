5936789deb5b5cad518cb72861edcc1306dc2663
gavazcal <2392@holbertonschool.com>
2021-03-01 13:09:56 +0000

    adding task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,15 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints the arg count
+ * @argc: arg count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
