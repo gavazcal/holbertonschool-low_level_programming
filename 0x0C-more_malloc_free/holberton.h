2d4d2328405fec6aaa01a9383464267c1a330964
gavazcal <2392@holbertonschool.com>
2021-03-04 20:04:20 +0000

    adding header file update

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,8 +5,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 /*PROTOTYPES*/
-void *malloc_chacked(unsigned int b);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
