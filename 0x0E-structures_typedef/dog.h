7c84747da2cc15405064e3c30db8a46d5183815d
gavazcal <2392@holbertonschool.com>
2021-03-08 17:58:14 +0000

    adding task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+/*PROTOTYPE*/
+int _putchar(char c);
+/*STRUCT*/
+/**
+ * struct dog - doggo struct
+ * @name: doggo name
+ * @age: doggo age
+ * @owner: doggo hooman
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner
+} dog_t;
+
+
+#endif
