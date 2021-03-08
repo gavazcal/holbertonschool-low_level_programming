5418a7c8b325f5b6b488749211075dff7b0723fd
gavazcal <2392@holbertonschool.com>
2021-03-08 18:02:33 +0000

    adding task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,16 @@ int _putchar(char c);
 
 typedef struct dog
 {
-char *name;
-float age;
-char *owner
-} dog_t;
+	char *name;
+	float age;
+	char *owner
+}dog_t;
+/*PROTOTYPES*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, flat age, char *owner);
+void free_dog(dog_t *d);
+
 
 
 #endif
