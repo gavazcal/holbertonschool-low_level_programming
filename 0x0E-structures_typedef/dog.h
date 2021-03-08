8a1c542e453f44685cbc6794871f3aa2ee7f8333
gavazcal <2392@holbertonschool.com>
2021-03-08 18:05:37 +0000

    fixed typos

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,12 +17,12 @@ typedef struct dog
 {
 	char *name;
 	float age;
-	char *owner
-}dog_t;
+	char *owner;
+} dog_t;
 /*PROTOTYPES*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, flat age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 
