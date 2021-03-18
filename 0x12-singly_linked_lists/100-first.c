3623d0ba61b0607904588364bc7dab8c641754ed
gavazcal <2392@holbertonschool.com>
2021-03-18 21:42:01 +0000

    Adding advanced task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_inb4 - prints a thing before the main
+ */
+void print_inb4(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n";
+}
+
