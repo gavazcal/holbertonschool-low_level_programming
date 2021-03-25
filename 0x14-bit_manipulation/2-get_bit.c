a37e9e7423fdbf206795128b01fa036c76aeb331
gavazcal <2392@holbertonschool.com>
2021-03-25 14:52:11 +0000

    adding task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the bit
+ * @index: the index
+ * Return: value at index or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
