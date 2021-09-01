1d15e5467ca565452f5150539e3f311019ec5eea
gavazcal <2392@holbertonschool.com>
2021-09-01 19:12:13 +0000

    adding task 0, correct output

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * linear_search - searches in linear search algorithm for a value
+ * @array: the elements to search through
+ * @size: the number of elements in the array
+ * @value: the element to search for
+ * Return: index of value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int idx, size_in_int;
+
+	size_in_int = size;
+
+	for (idx = 0; idx < size_in_int; idx++)
+	{
+		printf("Value checked array [%d] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
+	}
+	return (-1);
+}
