d5071fa638f5930710ee53e76e4aeac0de526570
gavazcal <2392@holbertonschool.com>
2021-09-01 19:13:34 +0000

    missing space in print statement

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (idx = 0; idx < size_in_int; idx++)
 	{
-		printf("Value checked array [%d] = [%d]\n", idx, array[idx]);
+		printf("Value checked array[%d] = [%d]\n", idx, array[idx]);
 		if (array[idx] == value)
 			return (idx);
 	}
